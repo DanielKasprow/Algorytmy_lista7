a38b5ee1208d6174dfba82cc6894e544d00b2877
DanielKasprow <trane031@gmail.com>
2020-11-29 13:38:48 +0100

    all function and inorder recursion

--- Daniel_Kasprów_lista_7/Daniel_Kasprów_lista_7.cpp
+++ Daniel_Kasprów_lista_7/Daniel_Kasprów_lista_7.cpp
@@ -1,20 +1,164 @@
-// Daniel_Kaspr√≥w_lista_7.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+int *key, *p, *leftt, *rightt,root=0,i = 0;;
+
+void bst_inorder(int x)
+{
+	if (x != 0)
+	{
+		bst_inorder(leftt[x]);
+		cout << key[x]<<" ";
+		bst_inorder(rightt[x]);
+	}
+}
+void bst_insert(int z)
+{
+	i++;
+	key[i] = z;
+
+	int x = root;//korzen drzewa
+	int y=0;
+	int k = key[i];//k nowy element
+	while (x != 0)
+	{
+		y = x;
+		if (k < key[y])
+		{
+			x = leftt[y];
+		}
+		else
+		{
+			x = rightt[y];
+		}
+	}
+
+	p[i] = y; // y jest ojcem nowego elementu
+	if (y == 0)
+	{
+		root = i;//gdy drzewo by≥oby puste
+	}
+
+	else
+	{
+		if (k < key[y])
+		{
+			leftt[y] = i;//przypisanie dziecka do lewego poddrzewa
+		}
+		else
+		{
+			rightt[y] = i;//przypisanie dziecka do prawego poddrzewa
+		}
+	}
+}
+
+int bst_member(int k)
+{
+	int x = root;
+	while (x != 0 && k != key[x])
+	{
+		if (k < key[x])
+		{
+			x = leftt[x];
+		}
+		else
+		{
+			x = rightt[x];
+		}
+	}
+	return x;
+}
+
+int bst_max(int x)
+{
+	while (rightt[x] != 0)
+	{
+		x = rightt[x];
+	}
+	return x;
+}
+
+int bst_min(int x)
+{
+	while (leftt[x] != 0)
+	{
+		x = leftt[x];
+	}
+	return x;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x,k;
+    std::cout << "Podaj wielkosc zbioru";
+	cin >> x;
+	p = new int [x+1];
+	leftt = new int [x+1];
+	rightt = new int [x+1];
+	key = new int [x+1];
+	char choice = 1;
+	for (x + 1; x > 0; x--)
+	{
+		p[x] = 0;
+		leftt[x] = 0;
+		rightt[x] = 0;
+	}
+	cout << "1: Bst-INORDER\n";
+	cout << "2: Bst-INSERT\n";
+	cout << "3: Bst-MEMBER\n";
+	cout << "4: Bst-MAX\n";
+	cout << "5: Bst-MIN\n";
+	cout << "0: Exit\n";
+	while (choice != 48)
+	{
+		cin >> choice;
+		switch (choice)
+		{
+		case 49:
+		{
+			bst_inorder(root);
+			cout << endl;
+		}
+		break;
+		case 50:
+		{
+			cout << "Podaj liczbe: ";
+			cin >> x;
+			bst_insert(x);
+		}
+		break;
+		case 51:
+		{
+			cout << "Podaj liczbe: ";
+			cin >> x;
+			k=bst_member(x);
+			if (key[k] == x)
+			{
+				cout << "ta liczba znajduje sie w indeksie: "<<k<<endl;
+			}
+			else
+			{
+				cout << "nie ma takiej liczby w drzewie\n";
+			}
+		}
+		break;
+		case 52:
+		{
+			cout << "Max: " << key[bst_max(root)]<<endl;
+		}
+		break;
+		case 53:
+		{
+			cout << "Min: " << key[bst_min(root)]<<endl;
+
+		}
+		break;
+		case 48:
+		break;
+		default:
+			cout << "Bledna opcja " << endl;
+			break;
+		}
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
